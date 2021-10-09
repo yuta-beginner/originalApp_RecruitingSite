3740bc504b28660a04c0009d16f7dd1b07795ab6
yuta_begginer <sy03141995139@yahoo.co.jp>
2021-10-09 13:02:17 +0900

    modify heroku error and change database

--- db/migrate/20211009035026_change_type_to_users.rb
+++ db/migrate/20211009035026_change_type_to_users.rb
@@ -1,5 +1,5 @@
 class ChangeTypeToUsers < ActiveRecord::Migration[5.2]
   def change
-    change_column :users, :tilype, :integer, null: false, default: 0
+    change_column :users, :type, :integer, null: false, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_211900) do
+ActiveRecord::Schema.define(version: 2021_10_09_035357) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
