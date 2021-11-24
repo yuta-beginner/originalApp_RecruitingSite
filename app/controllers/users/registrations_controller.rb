57a1d1845a325cea0984016de6269ced055569b8
yuta_begginer <sy03141995139@yahoo.co.jp>
2021-11-24 23:34:53 +0900

    modification of devise password update error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -61,7 +61,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super(resource)
   # end
   protected
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
+    def update_resource(resource, params)
+      resource.update_without_current_password(params)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,15 @@ class User < ApplicationRecord
   has_many :comments
   mount_uploader :image, ImageUploader
   mount_uploader :movie, VideoUploader
+
+  def update_without_current_password(params, *options)
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end  
 end
