449375c959d722f189421187182050f0f2ad6540
yuta_begginer <sy03141995139@yahoo.co.jp>
2021-11-26 21:12:17 +0900

    make forgot password mailer system on development stage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -61,4 +61,15 @@ Rails.application.configure do
 
   # deviseの設定
   config.action_mailer.default_url_options = { host: "localhost", port: ENV.fetch("PORT", 3000) }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV["GOOGLE_MAIL_ADDRESS"],
+    :password => ENV["GOOGLE_MAILER_PASSWORD"],
+    :authentication => 'login'
+  }
 end
